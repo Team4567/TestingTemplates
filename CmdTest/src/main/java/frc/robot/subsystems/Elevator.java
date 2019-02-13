9285aa04e0b31a4b2e7d8ba2de924b4cca0673cb
Joe Abbate <40615740+jabbate19@users.noreply.github.com>
2019-02-13 09:43:33 -0500

    Update Elevator.java

--- CmdTest/src/main/java/frc/robot/subsystems/Elevator.java
+++ CmdTest/src/main/java/frc/robot/subsystems/Elevator.java
@@ -18,20 +18,36 @@ import frc.robot.Constants;
  * Add your docs here.
  */
 public class Elevator extends Subsystem {
-  
+  private MotorCalculator posCalc;
+  private double diameter=0;
+  private double circ= Math.PI*diameter;
+  private double elevatorGearbox=184320;
   public TalonSRX t1;
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
   public Elevator(){
     t1=new TalonSRX(Constants.elevatorMainMC);
     t1.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
-    
+    //double maxOutChange, double Kp, double maxOutput, double minOutput, double minError
+    // ~184340 is one rotation
+    posCalc= new SimpleMotorP(.02,1,.1,100000);
     
   }
   
   
-  public void move(double value){
-    t1.set(ControlMode.PercentOutput, Math.min(Math.max(value,1),-1));
+  public void move(ElevatorPos pos){
+    switch(pos){
+      case ballLow:
+        posCalc.setSetpoint( 1 / circ * elevatorGearbox );
+      break;
+      case ballMed:
+        posCalc.setSetpoint( 2 / circ * elevatorGearbox );
+      break;
+      case ballHigh:
+        posCalc.setSetpoint( 3 / circ * elevatorGearbox );
+      break;
+    }
+    t1.set( ControlMode.PercentOutput, posCalc.getOutput( t1.getSelectedSensorPosition() ) );
     
   }
   @Override
